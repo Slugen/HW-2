c1a3629dd4698223f41a7bde730b7bf7c7dd4f07
HUAWEI <umargatiatulin14@gmail.com>
2024-07-26 17:53:48 +0500

    Домашка Урок переменные2

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,86 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Ураааааа!!!!");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        dog *= 2.0;
-        cat *= 2.0;
-        paper *= 2;
-        dog -= 3.5;
-        --cat;
-        paper -= 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println(friend);
-        friend += 2;
-        System.out.println(friend);
-        friend /= 7;
-        var frog = 3.5;
-        frog *= 10.0;
-        System.out.println(frog);
-        frog /= 3.5;
-        System.out.println(frog);
-        frog += 4.0;
-        System.out.println(frog);
-        System.out.println("-------------------------------------------------------------------------------------------");
-        var weightBoxer1 = 78.2;
-        var weightBoxer2 = 82.7;
-        var weightAll = weightBoxer1 + weightBoxer2;
-        System.out.println(weightAll);
-        var difference = weightBoxer2 % weightBoxer1;
-        System.out.println(difference);
-        System.out.println("-------------------------------------------------------------------------------------");
-        var allTime = 640;
-        var workerTime = 8;
-        var allWorkers = 80;
-        System.out.println("Если в компании работает " + allWorkers + " человек, то всего " + allTime + " часов работы может быть поделено между сотрудниками");
-        System.out.println("Всего работников в компании "+allTime/workerTime+" человек");
-        allWorkers=allWorkers+94;
-        System.out.println("Если в компании работает "+allWorkers+" человек,то всего "+allTime+" часов работы может быть поделено между сотрудниками");
+
+        byte bit= -15;
+        short sort=15141;
+        int iynt=-1234567;
+        long  lung=1234567891L;
+        float flot= 1.23456F;
+        double duble= 1.23456789D;
+        System.out.println("Значение переменной bit с типом byte равно "+bit);
+        System.out.println("Значение переменной sort с типом short равно "+sort);
+        System.out.println("Значение переменной iynt с типом int равно "+iynt);
+        System.out.println("Значение переменной lung с типом long равно "+lung);
+        System.out.println("Значение переменной flot с типом float равно "+flot);
+        System.out.println("Значение переменной duble с типом double равно "+duble);
+        System.out.println("____________-------------------_______________");
+        float ba=27.12F;
+        long bb= 987678965549L;
+        float bc=2.786F;
+        short bd=569;
+        short be=-159;
+        short bf=27897;
+        byte bg=67;
+        System.out.println("__________________----------------------______________________");
+        byte pavlovna=23;
+        byte sergeyevna=27;
+        byte andreyevna=30;
+        short allList=480;
+        int kajhdomuUcheniku=allList/(pavlovna+sergeyevna+andreyevna);
+        System.out.println("На каждого ученика рассчитано "+kajhdomuUcheniku+" листов бумаги");
+        System.out.println("__________________----------------------______________________");
+        byte twoMin=16;
+        int twentyMin=(twoMin*10);
+        int day=(twoMin*30)*24;
+        int threeDay=day*3;
+        int month=threeDay*10;
+        System.out.println("За 20 минут машина произвела "+twentyMin+" штук бутылок");
+        System.out.println("За сутки машина произвела "+day+" штук бутылок");
+        System.out.println("За 3 дня машина произвела "+threeDay+" штук бутылок");
+        System.out.println("За месяц машина произвела "+month+" штук бутылок");
+        System.out.println("______________________------------------------_______________________");
+        byte allPaint=120;
+        byte whitePaintClass=2;
+        byte brownPaintClass=4;
+        int allClasses=120/(brownPaintClass+whitePaintClass);
+        int allWhite=allClasses*whitePaintClass;
+        int allBrown=allClasses*brownPaintClass;
+        System.out.println("В школе, где "+allClasses+" классов, нужно "+allWhite+" банок белой краски и "+allBrown+" банок коричневой краски");
+        System.out.println("______________________------------------------_______________________");
+        int banana=80*5;
+        int milk=105*2;
+        int iceCream=100*2;
+        int egg=70*4;
+        int allWeightGram=banana+milk+iceCream+egg;
+        float allWeightKilo=(allWeightGram/ 1000);
+        System.out.println("Вес завтрака в граммах "+allWeightGram+", а в килограммах "+allWeightKilo);
+        System.out.println("_____________________-------------------------_________________________---");
+        short weightGramms=7000;
+        int twoHundredFiftyGramms=weightGramms/250;
+        int fiveHundredGramms=weightGramms/500;
+        System.out.println("Если спортсмен будет терять по 250гр в день то он сбросит 7 кг за "+twoHundredFiftyGramms+" дней");
+        System.out.println("Если спортсмен будет терять по 500гр в день то он сбросит 7 кг за "+fiveHundredGramms+" дней");
+        System.out.println("_____________________-------------------------_________________________---");
+        int salaryMasha=67760;
+        int salaryDenis=83690;
+        int salaryKristina=76230;
+        double percentIncrease=1.1;
+        double salaryMashaUp=salaryMasha*percentIncrease;
+        double salaryDenisUp=salaryDenis*percentIncrease;
+        double salaryKristinaUp=salaryKristina*percentIncrease;
+        byte year=12;
+        double percentYear=0.1;
+        double salaryMashaYearUp=(salaryMashaUp*percentYear)*year;
+        double salaryDenisYearUp=(salaryDenisUp*percentYear)*year;
+        double salaryKristinaYearUp=(salaryKristinaUp*percentYear)*year;
+        System.out.println("Маша теперь получает "+salaryMashaUp+" рублей. Годовой доход вырос на "+salaryMashaYearUp+" рублей.");
+        System.out.println("Денис теперь получает "+salaryDenisUp+" рублей. Годовой доход вырос на "+salaryDenisYearUp+" рублей.");
+        System.out.println("Кристина теперь получает "+salaryKristinaUp+" рублей. Годовой доход вырос на "+salaryKristinaYearUp+" рублей.");
+        System.out.println("Finished!!!");
 
 
-
+        
 
 
 
